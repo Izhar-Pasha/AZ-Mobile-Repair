0b2ee220c79bee1e78a2bb9e8f831f23fe84cbab
Izhar Pasha <mdizharpasha@07>
2024-05-22 01:54:34 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Home from "./Component/Home/Home.jsx";
 import Footer from './Component/Footer/Footer.jsx';
 import Mobile from './Component/Services/Mobile.jsx';
-import Laptop from './Component/Services/Laptop.jsx';
 import SmartWatch from './Component/Services/SmartWatch.jsx';
 import Headphones from './Component/Services/Headphones.jsx';
 import Earpods from './Component/Services/Earpods.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'normalize.css';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 
@@ -16,15 +17,16 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-        <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/Mobile' element={<Mobile />} />
           <Route path='/Watch' element={<SmartWatch />} />
-          <Route path='/Laptop' element={<Laptop />} />
           <Route path='/Headphones' element={<Headphones />} />
           <Route path='/Earpods' element={<Earpods />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
